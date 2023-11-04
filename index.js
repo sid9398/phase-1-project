2ec6076a46c10c0a759f4b0a0ed3a61768c13789
SidneyKing <sid8121@gmail.com>
2023-11-04 18:09:25 -0400

    finalizing search bar animation

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ function fetchData() {
     .then((data) => console.log(data))
 }
 
+const searchInputWrapper = document.querySelector(".search-input-wrapper")
+const searchInput = document.querySelector(".search-input input")
 const searchIcon = document.querySelector(".search-icon i")
 searchIcon.addEventListener('click', () => {
     searchIcon.parentElement.classList.add("change")
+    searchInputWrapper.classList.add("change")
+
+    setTimeout(() => {
+        searchInput.focus()
+    }, 1000)
 })
+
