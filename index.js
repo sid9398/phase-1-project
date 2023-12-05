d63c7d2c4c73ced442b94f4f1539c271478f8e60
SidneyKing <sid8121@gmail.com>
2023-12-04 21:57:03 -0500

    cleaning up code

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => fetchData())
-
 function fetchData() {
     fetch('https://api.tvmaze.com/shows')
     .then((resp) => resp.json())
-    .then((data) => console.log(data))
+    .then((json) => console.log(json))
 }
 
-// get elements by their ID
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
-
-// add event listener to the button
-searchButton.addEventListener('click', function() {
-  // get the search input value
-  const searchTerm = searchInput.value;
-  // do something with the search term (e.g. redirect to a search results page)
-  console.log(`Searching for "${searchTerm}"...`);
-});
-
-// add event listener to the input (if you want to trigger the search on Enter keypress)
-searchInput.addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
-    // simulate a button click to trigger the search
-    searchButton.click();
-  }
-});
+document.addEventListener("DOMContentLoaded", () => fetchData())
 
