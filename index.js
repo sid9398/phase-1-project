65d9c6a71566f1021d35e949339d20f0efd9b96a
SidneyKing <sid8121@gmail.com>
2023-12-06 17:17:12 -0500

    cleaning up code

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const showsURL = 'https://api.tvmaze.com/shows'
-const showNameEl = document.getElementById('show-name')
 const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
 
 fetch(showsURL)
     .then(response => response.json())
     .then(data => console.log(data))
-    //.then(responseJson => {
-      //  for (let {name} of responseJson){
-        //    const showName = document.createElement('p')
-          //  showName.innerText = name
-            //showNameEl.append(showName)
-        //}
-    //})
-
 
 // add event listener to the button
 searchInput.addEventListener('input', function() {
@@ -30,15 +20,7 @@ searchInput.addEventListener('input', function() {
     resultsList.appendChild(resultItem)
   })
     
-  });
-
-// add event listener to the input (if you want to trigger the search on Enter keypress)
-searchInput.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    // simulate a button click to trigger the search
-    searchButton.click();
-  }
-})
+  })
 
 
 
